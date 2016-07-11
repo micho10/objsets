a97612c1404128e6a7930c3a393e21d12634a826
micho <gatin13@yahoo.com>
2016-07-11 13:06:51 +0900

    Completed part 4, tying everything together

--- src/main/scala/objsets/TweetSet.scala
+++ src/main/scala/objsets/TweetSet.scala
@@ -201,14 +201,17 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-    lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  def filterByList(list: List[String]): TweetSet =
+    TweetReader.allTweets.filter(tweet => list.exists(set => tweet.text.contains(set)))
+
+  lazy val googleTweets: TweetSet = filterByList(google)
+  lazy val appleTweets: TweetSet = filterByList(apple)
   
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-     lazy val trending: TweetList = ???
+     lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
   }
 
 object Main extends App {
